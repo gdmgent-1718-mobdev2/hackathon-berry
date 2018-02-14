5325deac1a7d05c22a61ed776aa21c4129cedd60
gdm-1718-lukamary <lukamary@student.arteveldehs.be>
2018-02-14 14:28:10 +0100

    react-navigation added

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import { initFirebase } from './utils/firebaseInit';
+
 
 export default class App extends React.Component {
+
+  constructor(){
+    super();
+
+  }
+
+  componentWillMount(){
+    //firebase wordt hier ingeladen   
+    initFirebase(firebase);
+
+    //test genereren firebase key
+    var newPostKey = firebase.database().ref().child('testTable').push().key;
+    console.log(newPostKey);
+    
+  }
+
+  componentDidMount() {
+
+  }
+  
   render() {
     return (
       <View style={styles.container}>
