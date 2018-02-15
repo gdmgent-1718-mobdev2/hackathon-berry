911b849f966800e90c82833fa29cbbd7ef5a9b23
Nielske <niels.vancauwenberghe@gmail.com>
2018-02-16 00:35:34 +0100

    Mijn tuin toegevoegd

--- screens/MijnTuin.js
+++ screens/MijnTuin.js
@@ -5,52 +5,84 @@ import firebase from '../config/firebase';
 
 //main class
 export default class MijnTuinScreen extends React.Component {
-  constructor(props) {
+    constructor(props) {
 
-    super(props);
-    this.state = {
+        super(props);
+        this.state = {
 
-      gebruiker: '',
+            gebruiker: '',
+            tuinPic: '',
+            openingsuren: '',
+        }
+
+
     }
+    static navigationOptions = {
+        title: 'Mijn Tuin',
+    };
 
 
-  }
-  static navigationOptions = {
-    title: 'Home',
-  };
+    componentWillMount() {
 
-  //load data of gebruiker1 from database
-  componentWillMount() {
-    var database = firebase.database().ref('Gebruikers/gebruiker1/');
-    console.log(database);
-    database.once('value', function (snapshot) {
-      gebruiker = snapshot.val();
-    });
+        let self = this;
 
-  }
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={[{ key:'1', foto: require('../images/Landschap.jpg') }, { key:'2', foto: require('../images/Landschap.jpg') }]}
-          renderItem={({ item }) => {return(<FlatListItem imageUrl={item.foto} />);}}
-        />
+        //load data of gebruiker1 from database
+        var database = firebase.database().ref('Gebruikers/gebruiker1/');
+        console.log(database);
+        database.once('value', function (snapshot) {
+            self.setState({gebruiker : snapshot.val()});
+            self.setState({openingsuren : snapshot.val().winkel.openingsuren});
+            
+        });
 
-      </View>
-    );
-  }
+        //load foto tuin van databank
+        let storage = firebase.storage();
+        let tuinPicUrl = 'KiFthOFtGMSQRPRxyeHVNwRx2Ab2.jpg';
+        var tuinReference = storage.refFromURL('gs://hackathon-berry.appspot.com/images/tuinPics/' + tuinPicUrl);
+        
+        tuinReference.getDownloadURL()
+            .then((url) => {
+                self.setState({ tuinPic: { uri: url } });
+                console.log(this.state.tuinPic.uri);
+                 
+            })
+            .catch(function (error) {
+                // Handle Errors here.
+                console.log('couldnt get photo ');
+                // ...
+            });
+
+    }
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={styles.container}>
+                <Image style={styles.tuinPic} source={{uri: this.state.tuinPic.uri}} />
+                <Text>Adres: {this.state.gebruiker.straat} {this.state.gebruiker.huisnummer} {this.state.gebruiker.postcode} {this.state.gebruiker.stad}</Text>
+                <Text>Maandag: {this.state.openingsuren.maandag}</Text>
+                <Text>Dinsdag: {this.state.openingsuren.dinsdag}</Text>
+                <Text>Woensdag: {this.state.openingsuren.woensdag}</Text>
+                <Text>Donderdag: {this.state.openingsuren.donderdag}</Text>
+                <Text>Vrijdag: {this.state.openingsuren.vrijdag}</Text>
+                <Text>Zaterdag: {this.state.openingsuren.zaterdag}</Text>
+                <Text>Zondag: {this.state.openingsuren.zondag}</Text>
+                
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 300,
-    height: 100,
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    tuinPic: {
+        height: 200,
+        width: 200,
+        
+    }
+   
 });
